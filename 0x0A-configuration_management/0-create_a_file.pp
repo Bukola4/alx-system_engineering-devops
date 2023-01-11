2e784d75833fe41b45ca409312addbb40d25a9b1
Bukola4 <bukky.ruq@gmail.com>
2023-01-11 10:25:40 -0800

    task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creating a file with required attribute
+file { '/tmp/school':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
+
