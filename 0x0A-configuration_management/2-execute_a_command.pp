201ddcaeb08ef137f1c3bfe62daad1550c644152
Bukola4 <bukky.ruq@gmail.com>
2023-01-11 11:11:15 -0800

    task

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,12 @@
+# Killing an external process
+exec { 'kill_killmenow':
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  before  => Service['killmenow'],
+}
+
+service { 'killmenow':
+  ensure => 'stopped',
+  enable => false,
+}
+
